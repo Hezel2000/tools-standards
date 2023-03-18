b1c7885588142dea480029c04e80895d7db26a5e
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2023-03-18 15:58:38 +0100

    x

--- Welcome.py
+++ Welcome.py
@@ -4,4 +4,4 @@ st.header('Welcome')
 
 st.write('Check out our standards here.')
 
-st.sidebar.st.markdown(f'''<a href='http://geoplatform.de'><button style="background-color:LightYellow;">geoplatform.de</button></a>''', unsafe_allow_html=True)
+st.sidebar.markdown(f'''<a href='http://geoplatform.de'><button style="background-color:LightYellow;">geoplatform.de</button></a>''', unsafe_allow_html=True)
