20202a4d2e24621b64eb75101c934033fcb29f70
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2023-03-18 15:57:49 +0100

    x

--- Welcome.py
+++ Welcome.py
@@ -4,4 +4,4 @@ st.header('Welcome')
 
 st.write('Check out our standards here.')
 
-st.sidebar.write('Back to <a href='http://geoplatform.de'geoplatform.de</a>)
+st.sidebar.st.markdown(f'''<a href='http://geoplatform.de'><button style="background-color:LightYellow;">geoplatform.de</button></a>''', unsafe_allow_html=True)
