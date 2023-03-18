fa0abe37d96105335f9df5b4a8ca4be99fd64d2a
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2023-03-18 15:59:40 +0100

    x

--- Welcome.py
+++ Welcome.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.header('Welcome')
 
-st.write('Check out our standards here.')
+st.write('Check out our standards from the sidebar.')
 
 st.sidebar.markdown(f'''<a href='http://geoplatform.de'><button style="background-color:LightYellow;">Back to GeoPlatform</button></a>''', unsafe_allow_html=True)
