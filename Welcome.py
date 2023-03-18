41b0696e7d42ac0d9496d242ec54dd85208346b7
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2023-03-18 15:55:41 +0100

    x

--- Welcome.py
+++ Welcome.py
@@ -2,4 +2,6 @@ import streamlit as st
 
 st.header('Welcome')
 
-st.write('Here')
+st.write('Check out our standards here.')
+
+st.sidebar.write('Back to <a href='http://geoplatform.de'geoplatform.de</a>)
