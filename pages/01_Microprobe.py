aa79beb8778b4d4f2b58fd9e67076d73f28ef463
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2023-03-20 13:18:55 +0100

    x

--- pages/01_Microprobe.py
+++ pages/01_Microprobe.py
@@ -1,18 +1,24 @@
 import streamlit as st
 import pandas as pd
-st.sidebar.markdown(f'''<a href='http://geoplatform.de'><button style="background-color:LightYellow;">Back to GeoPlatform</button></a>''', unsafe_allow_html=True)
+st.sidebar.button(f'''<a href='http://geoplatform.de'><button style="background-color:LightYellow;">Back to GeoPlatform</button></a>''', unsafe_allow_html=True)
 
 df = pd.read_csv('data/epma_standards.csv')
 elements = df.columns[10:129].tolist()
 
-colel1_1, colel1_2 = st.columns([1,3])
-with colel1_1:
-    st.session_state.el1 = st.selectbox('Element', elements, index=8)
-with colel1_2:
-    st.session_state.el1_range = st.slider('sel', .0, 100.0, (0., 100.))
+tab1, tab2 = st.tabs(['Elements', 'Holder Images'])
 
-fil = (df[st.session_state.el1] > st.session_state.el1_range[0]) & (df[st.session_state.el1] < st.session_state.el1_range[1])
-st.dataframe(df[fil])
+with tab1:
+    colel1_1, colel1_2 = st.columns([1,3])
+    with colel1_1:
+        st.session_state.el1 = st.selectbox('Element', elements, index=8)
+    with colel1_2:
+        st.session_state.el1_range = st.slider('sel', .0, 100.0, (0., 100.))
 
-with st.expander('full table'):
-    st.dataframe(df)
+    fil = (df[st.session_state.el1] > st.session_state.el1_range[0]) & (df[st.session_state.el1] < st.session_state.el1_range[1])
+    st.dataframe(df[fil])
+
+    with st.expander('The entire standards table'):
+        st.dataframe(df)
+    
+with tab2:
+    st.write('\test')
