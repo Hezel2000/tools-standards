e1ed73245a222a86db853b5a163af26408c0dbce
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2023-03-20 14:52:16 +0100

    x

--- pages/01_Microprobe.py
+++ pages/01_Microprobe.py
@@ -18,10 +18,9 @@ with tab1:
 
     df1 = st.session_state.df
     fil = (df1[st.session_state.el1] > st.session_state.el1_range[0]) & (df1[st.session_state.el1] < st.session_state.el1_range[1])
-    st.dataframe(df1[fil])
+    df_el_series = df1[fil][st.session_state.el1]
     st.dataframe(df1[fil].drop(st.session_state.el1, axis=1))
-    #st.dataframe(df1[fil].insert(3, st.session_state.el1, df_el_series))
-    #df_el_series = df1[st.session_state.el1]
+    st.dataframe(df1[fil].drop(st.session_state.el1, axis=1).insert(3, st.session_state.el1, df_el_series))
 
     with st.expander('The entire standards table'):
         st.dataframe(st.session_state.df)
